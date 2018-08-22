55bd47e71638a0f6e942a628646670ed3681ca46
mperdeck@gmail.com <mperdeck@gmail.com>
2018-08-22 22:02:00 +1000

    initial classlibrary1 solution

---
+++ ClassLibrary1/ClassLibrary1_2/Class1_2.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ClassLibrary1_2
+{
+    public class Class1_2
+    {
+        public void Function2()
+        {
+            Class1.Function1();
+        }
+    }
+}
