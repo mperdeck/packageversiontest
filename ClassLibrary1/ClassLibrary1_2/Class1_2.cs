5b56c26f9fe90570b8c07dde4d5ddcdb22269332
mperdeck@gmail.com <mperdeck@gmail.com>
2018-08-23 21:37:38 +1000

    solution is to explicitly import ClassLibrary1 in ClassLibirary2

--- ClassLibrary1/ClassLibrary1_2/Class1_2.cs
+++ ClassLibrary1/ClassLibrary1_2/Class1_2.cs
@@ -5,8 +5,8 @@ namespace ClassLibrary1_2
 {
     public class Class1_2
     {
-        public void Function2()
-        {
+        public static void Function1_2a()
+        { 
             Class1.Function1();
         }
     }
--- ClassLibrary2/ClassLibrary2/Class2.cs
+++ ClassLibrary2/ClassLibrary2/Class2.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using ClassLibrary1_2;
 
 namespace ClassLibrary2
 {
     public class Class2
     {
+        public void Function2()
+        {
+            Class1_2.Function1_2a();
+        }
     }
 }
